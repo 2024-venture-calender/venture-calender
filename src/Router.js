801bc553c5e0107317240725039f2c43aef01bef
inredzero <caesete0524@naver.com>
2024-05-01 23:00:04 +0900

    first settings

---
+++ src/Router.js
@@ -0,0 +1,17 @@
+// src/Router.js
+
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Main from './pages/Main/Main';
+
+const Router = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/main" element={<Main />} />
+            </Routes>
+        </BrowserRouter>
+    );
+};
+
+export default Router;
