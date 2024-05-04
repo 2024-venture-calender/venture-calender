19485bb9cbc7be7f2222e792b8acd43dd318f4a5
ddoddo1228 <135615995+ddoddo1228@users.noreply.github.com>
2024-05-04 22:40:14 +0900

    Add files via upload

--- src/Router.js
+++ src/Router.js
@@ -10,22 +10,13 @@ import Layout from './layouts/Layout';
 const Router = () => {
     return (
         <div>
-            <Header />
-            
-            {/* 님 근데 님은 왜 layout 파일이 있음? 
-                일단 ㅇㅋ 저 에러가 만약 어처구니 없는 에러면
-                혼날줄 아셈 ^^ 오 누가 봐도 임포트 경로 에런데
-                안...했....네? 아니 여기위에 ㄷㅏ 1이잖아 글고 2랑 3은 어케해?또만들어?
-                일단... 기달려
-            */}
+            {/* <Header /> */}
             <Routes>
-                <Route path="/" element={ <Layout/> }>
-                    <Route index element={ <Main/> }/>
-                    <Route path="/containerCombine" element={<ContainerCombine />} />
+                <Route path="/" element={<ContainerCombine/>}>
+                    <Route path="/main" element={<Main/>}/>
                 </Route>
             </Routes>
-            <Map />
-            <Footer />
+            {/* <Footer/> */}
         </div>
     );
 }
