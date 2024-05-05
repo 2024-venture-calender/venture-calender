67ce882af285b302ca8a830fdd73f5a4dac98525
do0ik <133643805+do0ik@users.noreply.github.com>
2024-05-05 19:04:59 +0900

    [develop/ListToMaster] (#17) 메인-(캘린더, *목록*)-상세 페이지 연결
    * 메인 페이지 Initial Commit
    
    * Add files via upload
    
    #4 캘린더
    
    * Add files via upload
    
    * (#5) CSS 모듈 클래스명 일부 수정
    
    * (#5) CSS 모듈 클래스명 일부 수정
    
    * CalendarPage => Layout 하위 페이지 Outlet으로 연결
    
    * #10
    
    #10 커밋.,.
    
    * #10
    
    #10 커밋.,.
    
    * 컴포넌트 복구
    
    컴포넌트 복구
    
    * Main 복구
    
    * EventList - #6 목록출력, #9 검색기능
    
    * page 복구
    
    * 주석 제거
    
    * (#11) 메인-캘린더 연결 및 캘린더 디자인 보완
    
    * (#13) 메인-캘린더-상세 페이지 연결
    캘린더
    - 이벤트 호버 시 커서 포인터
    
    상세 페이지
    - 컴포넌트 통합
    - CSS 모듈화
    - CSS 일부 수정
    
    * 미사용 이미지 삭제
    
    * 미사용 코드 삭제
    
    * (#17) List - Master(Main, Calendar, Details) 초기 병합
    - /event 최상위 루트 접속 => 캘린더
    - /event/calendar => 캘린더
    - /event/list => 목록
    
    * (#17) 버튼 클릭을 통해 출력 형식 변경 기능 추가
    Issue: EventLIst.css 모듈화 필요
    
    ---------
    
    Co-authored-by: Sunwoo Sim <124691590+k168ww@users.noreply.github.com>
    Co-authored-by: k168ww <k168ww@gmail.com>
    Co-authored-by: SeYeon <syp9908@gmail.com>

--- src/Router.js
+++ src/Router.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Layout from './layouts/Layout';
 import Main from './pages/Main/Main';
-import EventCalendar from './pages/Calendar/EventCalendar';
+import EventCalendar from './pages/EventCalendar/EventCalendar';
+import EventList from './pages/EventList/EventList';
 import EventDetails from './pages/Events/EventDetails';
 
 const Router = () => {
     return (
         <BrowserRouter>
+            
             <Routes>
                 <Route path="/" element={<Layout />}>
                     <Route index element={<Main />}/>
                     <Route path="/event">
                         <Route index element={<EventCalendar />} />
+                        <Route path="calendar" element={<EventCalendar />} />
+                        <Route path="list" element={<EventList />} />
                         <Route path=":eventId" element={<EventDetails />} />
                     </Route>
                 </Route>
             </Routes>
+            
         </BrowserRouter>
     );
 };
