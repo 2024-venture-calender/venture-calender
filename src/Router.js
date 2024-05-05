62253efcddda1252d5ed562f42928927786e39bf
do0ik <oulwind00@naver.com>
2024-05-05 18:00:00 +0900

    (#17) List - Master(Main, Calendar, Details) 초기 병합 - /event 최상위 루트 접속 => 캘린더 - /event/calendar => 캘린더 - /event/list => 목록

--- src/Router.js
+++ src/Router.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Layout from './layouts/Layout';
 import Main from './pages/Main/Main';
 import EventCalendar from './pages/Calendar/EventCalendar';
-// import EventList from './pages/EventList/EventList';
+import EventList from './pages/EventList/EventList';
 import EventDetails from './pages/Events/EventDetails';
 
 const Router = () => {
@@ -15,9 +16,10 @@ const Router = () => {
                     <Route index element={<Main />}/>
                     <Route path="/event">
                         <Route index element={<EventCalendar />} />
+                        <Route path="calendar" element={<EventCalendar />} />
+                        <Route path="list" element={<EventList />} />
                         <Route path=":eventId" element={<EventDetails />} />
                     </Route>
-                    {/* <Route path='/eventlist' element={<EventList/>}/> */}
                 </Route>
             </Routes>
             
